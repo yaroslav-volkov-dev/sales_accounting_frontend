a62efdcfc59761ea745f15e06d915b7291c4d0f0
yaroslav-volkov-dev <volkov.yaroslav94@gmail.com>
2024-12-16 22:50:00 +0200

    moved api to new database

---
+++ src/constants/endpoints.js
@@ -0,0 +1,9 @@
+export const ENDPOINTS = Object.freeze({
+  REGISTER: '/auth/register',
+  LOGIN: '/auth/login',
+  AUTH_ME: '/auth/me',
+  ME: '/auth/me',
+  UPLOAD: '/upload',
+  PRODUCTS: '/products',
+  CATEGORIES: '/categories',
+});
