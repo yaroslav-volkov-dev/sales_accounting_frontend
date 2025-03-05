5609e7e43e41d32e8555dc21178ea071b2535012
yaroslav-volkov-dev <volkov.yaroslav94@gmail.com>
2025-03-05 18:22:13 +0200

    added unresolved files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { router } from './router/router.tsx';
+
+const queryClient = new QueryClient();
+
+export const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </QueryClientProvider>
+  );
+};
---
+++ src/types/products-query.types.ts
@@ -0,0 +1,6 @@
+export enum ProductsQueryFilterKey {
+  CATEGORIES_IDS = 'categoriesIds',
+  SUPPLIERS_IDS = 'suppliersIds',
+  WITHOUT_CATEGORY = 'withoutCategory',
+  WITHOUT_SUPPLIER = 'withoutSupplier'
+}
