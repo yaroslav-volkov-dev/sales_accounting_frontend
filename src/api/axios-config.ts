0778a834767f481c76ab16834ad6ca4b493e59c7
yaroslav-volkov-dev <volkov.yaroslav94@gmail.com>
2025-03-16 21:05:44 +0200

    rename project to sales_accounting_frontend and update lint script accordingly; remove commented BASE_URL in axios config

--- src/api/axios-config.ts
+++ src/api/axios-config.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import { LOCAL_STORAGE_KEY } from '../constants/local-storage-keys.ts'
 
-// export const BASE_URL = 'https://shopping-list-backend-wine.vercel.app';
 export const BASE_URL = 'http://localhost:3000/'
 
 export const axiosInstance = axios.create({
